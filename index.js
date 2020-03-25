215534ac3fea204bd13ad6f1ba69d35a4842f38d
Mercia <merciarandrianome@gmail.com>
2020-03-25 15:01:42 +0100

    configure mysql

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const mysql = require("mysql");
+const settings = require("./settings.json");
+
+const sqlConfig = settings.sqlConfig;
 
 const app = express();
 
@@ -6,7 +10,18 @@ app.listen(3000, () => {
     console.log("SERVER STARTED...");
 });
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+
+    sqlConnection.query("SELECT id, email, firstname, lastname, birthdate FROM node_users", (error, result) => {
+        if (error) {
+            console.log("ERROR :", error.code);
+        } else {
+            console.log("RESULT :", result);      
+        }
+        sqlConnection.end();
+    });
+	
     res.send({
         id: 1,
         email: "merciarandrianome@gmail.com",
@@ -15,3 +30,5 @@ app.get("/", (req,res) => {
         birthdate: new Date(1990, 9, 6),
     });
 });
+
+
